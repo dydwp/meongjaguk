f1e8c7faa5a672ce79b24149c255897c700b59d6
seokjin <hepatica387@gmail.com>
2026-09-25 00:55:47 +0900

    fix: 데이터베이스명 수정
    (cherry picked from commit 13126397500c2fb308c9ea028a13b88ef9fc1506)

--- sql/db 테이블 생성 sql/0.DB 생성.sql
+++ sql/db 테이블 생성 sql/0.DB 생성.sql
@@ -4,7 +4,7 @@
 -- DBMS     : MySQL
 -- =========================================================
 
-CREATE DATABASE IF NOT EXISTS mungjaguk
+CREATE DATABASE IF NOT EXISTS meongjaguk
     DEFAULT CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
