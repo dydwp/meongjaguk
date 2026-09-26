80317f9fe57316a1ca09cdbbc050746c7f340ccb
seokjin <hepatica387@gmail.com>
2026-09-26 18:18:57 +0900

    chore: 반려견 테스트 이미지 제거

--- src/main/java/com/mungjaguk/app/controller/RouteController.java
+++ src/main/java/com/mungjaguk/app/controller/RouteController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.mungjaguk.app.dto.RouteDto;
 import com.mungjaguk.app.entity.Route;
 import com.mungjaguk.app.service.RouteService;
 
